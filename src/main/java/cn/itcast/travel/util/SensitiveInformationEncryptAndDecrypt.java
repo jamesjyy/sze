644373246c503f105d83717087c558fd78e81497
gezhenyu <18838233863@163.com>
2019-08-28 11:17:46 +0800

    数据库连接测试

--- src/main/java/cn/itcast/travel/util/SensitiveInformationEncryptAndDecrypt.java
+++ src/main/java/cn/itcast/travel/util/SensitiveInformationEncryptAndDecrypt.java
@@ -77,7 +77,7 @@ public class SensitiveInformationEncryptAndDecrypt {
             	sql = "select ID id,IDNO idno,IDDOC_NAME name from "+tables.trim();
             	//创建prepareStatement
     			pst = con.prepareStatement(sql);
-                //3.ResultSet类，用来存放获取的结果集！！
+                //3.ResultSet类，用来存放获取的结果集！！！
                 rs = pst.executeQuery();
                 while(rs.next()){
                 	Map paramMap = new HashMap<>();
